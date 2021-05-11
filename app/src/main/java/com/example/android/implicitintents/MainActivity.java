25e30945b72abc05be231ef09bc465145a36f665
omkar01 <omkargujja01@gmail.com>
2021-05-11 16:39:16 +0530

    4.1b Exercise completed

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -15,10 +15,16 @@
  */
 package com.example.android.implicitintents;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,9 +42,10 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenWebpageButton(View v) {
         // TODO (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
+        String url = "https://www.udacity.com";
 
         // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
-        Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
+        openWebPage(url);
     }
 
     /**
@@ -79,6 +86,14 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a method called openWebPage that accepts a String as a parameter
     // Do steps 2 - 4 within openWebPage
+    public void openWebPage(String url){
+        Uri udacityUri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW,udacityUri);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+
+    }
 
         // TODO (2) Use Uri.parse to parse the String into a Uri
 
