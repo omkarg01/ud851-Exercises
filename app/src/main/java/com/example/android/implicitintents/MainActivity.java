c11a52188bf977c28f6b75987ec65118bdcb4a06
omkar01 <omkargujja01@gmail.com>
2021-05-11 17:05:50 +0530

    4.2b Exercise completed

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -49,11 +49,19 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenAddressButton(View v) {
         // TODO (5) Store an address in a String
+        String addressString = "Dubai";
+
+        // COMPLETED (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo")
+                .path("0,0")
+                .appendQueryParameter("q", addressString);
 
         // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
 
+        Uri uri = builder.build();
         // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        showMap(uri);
     }
 
     /**
@@ -119,6 +127,17 @@ public class MainActivity extends AppCompatActivity {
         // TODO (3) Set the data of the Intent to the Uri passed into this method
 
         // TODO (4) Verify that this Intent can be launched and then call startActivity
+
+    public void showMap(Uri mapUri){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(mapUri);
+
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
 
 
 }
