29524860f7d536986575096e563480e8285623ff
omkar01 <omkargujja01@gmail.com>
2021-05-12 15:12:51 +0530

    5.3 Solution

--- app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.lifecycle;
 
 import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
